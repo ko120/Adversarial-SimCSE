b52acbb99bcb1df4ac679a31411edefa46f042b3
ko120 <61360584+ko120@users.noreply.github.com>
2023-11-04 18:45:42 -0400

    Update train.py

--- train.py
+++ train.py
@@ -251,7 +251,7 @@ def main():
     wandb.init()
     cfig = wandb.config
     alpha = cfig.alpha
-
+    
     parser = HfArgumentParser((ModelArguments, DataTrainingArguments, OurTrainingArguments))
     if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
         # If we pass only one argument to the script and it's the path to a json file,
@@ -265,12 +265,12 @@ def main():
         and os.listdir(training_args.output_dir)
         and training_args.do_train
         and not training_args.overwrite_output_dir
-    )
+    ):
         raise ValueError(
             f"Output directory ({training_args.output_dir}) already exists and is not empty."
             "Use --overwrite_output_dir to overcome."
         )
-
+    training_args.output_dir += str( alpha)
     # Setup logging
     logging.basicConfig(
         format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
@@ -579,12 +579,14 @@ def main():
                 for key, value in sorted(results.items()):
                     logger.info(f"  {key} = {value}")
                     writer.write(f"{key} = {value}\n")
-
-    os.system(f'python3.7 evaluation.py \
-    --model_name_or_path /content/Adversarial-SimCSE/result/my-sup-simcse-bert-base-uncased \
-    --pooler cls \
-    --task_set sts \
-    --mode test')
+    part1 = "python evaluation.py"
+    part2 = "--model_name_or_path princeton-nlp/sup-simcse-bert-base-uncased" +str(alpha)
+    part3 = "--pooler cls"
+    part4 = "--task_set sts"
+    part5 = "--mode test"
+    command = f"{part1} {part2} {part3} {part4} {part5}"
+    
+    os.system(command)
     return results
 
 def _mp_fn(index):
@@ -598,7 +600,7 @@ if __name__ == "__main__":
         sweep_config = dict()
         sweep_config['method'] = 'grid'
         sweep_config['metric'] = {'name': 'test_accuracy', 'goal': 'maximize'}
-        sweep_config['parameters'] = {'alpha' : {'values' : [0.1,0.3,0.5,0.7,0.9]}, 'K_iter':[3]}
+        sweep_config['parameters'] = {'alpha' : {'values' : [0.5]}, 'K_iter':{'values':[3]}}
         sweep_id = wandb.sweep(sweep_config, project = 'Adversarial_SimCSE')
         wandb.agent(sweep_id, main)
 
