0f9ff8b30f2bfb43491c399841423ca4e29ec264
ko120 <61360584+ko120@users.noreply.github.com>
2023-11-08 15:18:48 -0500

    Update trainers.py

--- simcse/trainers.py
+++ simcse/trainers.py
@@ -9,6 +9,7 @@ import shutil
 import time
 import warnings
 from pathlib import Path
+import pdb
 import importlib.util
 from packaging import version
 from transformers import Trainer
@@ -113,7 +114,7 @@ class CLTrainer(Trainer):
                 batch[k] = batch[k].to(self.args.device)
             with torch.no_grad():
                 outputs = self.model(**batch, output_hidden_states=True, return_dict=True, sent_emb=True)
-                pooler_output = outputs.pooler_output
+                pooler_output = outputs.last_hidden_state[:,0,:]
             return pooler_output.cpu()
 
         # Set params for SentEval (fastmode)
